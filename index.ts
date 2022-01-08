3384081f4478b0039397d48dcad1a2a8d331b05e
chethanck <chethanckm66@gmail.com>
2022-01-08 09:19:46 +0530

    Adding interface

--- index.ts
+++ index.ts
@@ -2,16 +2,22 @@ import axios from "axios";
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+interface Todo { // Used to define a structure of an object
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then(response => {
-  const todo = response.data;
+  const todo = response.data as Todo; // Add interface Todo
   
-  const ID = todo.ID;
-  const title = todo.Title;
-  const finished = todo.Completed;
+  const id = todo.id;
+  const title = todo.title;
+  const completed = todo.completed;
   console.log(`
-    The Todo with ID: ${ID}
+    The Todo with ID: ${id}
     The Todo with title: ${title}
-    Is Todo completed: ${finished}
+    Is Todo completed: ${completed}
   `);
   
 });
